a06e51df9d44118487cb3a6b97379864e1d909cf
enthye <kxu015@aucklanduni.ac.nz>
2018-09-29 23:36:34 +1200

    login prototype

--- src/namesayer/login/LoginController.java
+++ src/namesayer/login/LoginController.java
@@ -1,5 +1,34 @@
-package namesayer.login;
+package namesayer;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import namesayer.login.User;
 
 public class LoginController {
+
+    @FXML
+    private ListView<User> userList;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button newUserButton;
+
+    @FXML
+    void loginButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newUserButtonClicked(ActionEvent event) {
+
+    }
 
 }
