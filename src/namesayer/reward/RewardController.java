dc7c71016ac6583f80ab497f6a909ad8847a4693
Eric <leung.eric24@gmail.com>
2018-10-12 18:54:15 +1300

    Fixing styles and selectables

--- src/namesayer/reward/RewardController.java
+++ src/namesayer/reward/RewardController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
