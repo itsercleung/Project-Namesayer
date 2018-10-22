dd20d486b3964f7b6811944965882dcb8f24782d
enthye <kxu015@aucklanduni.ac.nz>
2018-10-22 21:46:03 +1300

    authors and javadoc

--- src/namesayer/MainController.java
+++ src/namesayer/MainController.java
@@ -9,16 +9,19 @@ import java.io.IOException;
 /**
  * MainController: Initializes functionality of the main sidebar menu and other components. Main menu doesn't have any
  * functionality other than to welcome the user and provide sidemenu to navigate to other controllers
+ * @author Eric Leung, Kevin Xu
  */
 public class MainController extends NameSayerMenuController implements Initializable {
 
-    //Method to delete all existing temp files within the folder (usually deleted on startup)
+    /**
+     * Method to delete all existing temp files within the folder (usually deleted on startup)
+     */
     public void deleteTemp() {
         File temp = new File("temp/");
         if (temp.exists() && temp.isDirectory()) {
             String[] entries = temp.list();
-            for(String s: entries){
-                File currentFile = new File(temp.getPath(),s);
+            for (String s : entries) {
+                File currentFile = new File(temp.getPath(), s);
                 currentFile.delete();
             }
         }
@@ -44,7 +47,7 @@ public class MainController extends NameSayerMenuController implements Initializ
         new File("./data/usernames").mkdirs();
 
         // Reward and help Popup icons
-        IconLoader iconLoader = new IconLoader(user,rewardButton,helpButton,exitButton);
+        IconLoader iconLoader = new IconLoader(user, rewardButton, helpButton, exitButton);
         iconLoader.loadMenuIcons();
     }
 }
